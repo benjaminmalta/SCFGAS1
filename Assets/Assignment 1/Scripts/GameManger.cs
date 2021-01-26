458afbf4ac1f175f90ea0841449ffc1ce11d1a3b
benjaminmalta <56227477+benjaminmalta@users.noreply.github.com>
2021-01-26 17:04:04 +0100

    Added AStarGrid To Brief and some prefabs

---
+++ Assets/Assignment 1/Scripts/GameManger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Scan() {
+        GameObject.Find("AStarGrid").GetComponent<AstarPath>().Scan();
+    }
+
+    public void AddObstacle()
+    {
+
+    }
+
+    public void AddAI() 
+    { 
+    
+    }
+
+    public void StartExample()
+    {
+
+    }
+
+}
